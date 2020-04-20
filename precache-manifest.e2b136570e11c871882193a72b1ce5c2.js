b6982b75fdd5fb7a599cc5ddf64e3b742ddf3d2d
Vasyl <muran.wasyl@ukr.net>
2020-04-20 21:08:53 +0300

    Updates

---
+++ precache-manifest.e2b136570e11c871882193a72b1ce5c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3303690e8581d2af4c6dfb2470f70ec6",
+    "url": "/contacts/index.html"
+  },
+  {
+    "revision": "913e77e0c48a7af1f16f",
+    "url": "/contacts/static/css/main.690b390a.chunk.css"
+  },
+  {
+    "revision": "6dddfec0ed40cdd482c2",
+    "url": "/contacts/static/js/2.90049aac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contacts/static/js/2.90049aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913e77e0c48a7af1f16f",
+    "url": "/contacts/static/js/main.f0acbc0f.chunk.js"
+  },
+  {
+    "revision": "07af4bc0048af40d0863",
+    "url": "/contacts/static/js/runtime-main.500ec2f5.js"
+  },
+  {
+    "revision": "c3ec00650365a1a22662480b51ff315f",
+    "url": "/contacts/static/media/search.c3ec0065.svg"
+  }
+]);
